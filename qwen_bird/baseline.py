f4b1428691b89dcffed6f80561a830d60bd3426c
sangh <samuele.angheben@gmail.com>
2025-05-29 13:44:03 +0200

    remove flash attention

--- qwen_bird/baseline.py
+++ qwen_bird/baseline.py
@@ -26,14 +26,6 @@ model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
     "Qwen/Qwen2.5-VL-3B-Instruct", torch_dtype="auto", device_map="auto"
 )
 
-# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
-model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-3B-Instruct",
-    torch_dtype=torch.bfloat16,
-    attn_implementation="flash_attention_2",
-    device_map="auto",
-)
-
 # default processer
 processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
 
