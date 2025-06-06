afadcccd4931f9aeaf318967e8516b29ba6e8c5d
sangh <samuele.angheben@gmail.com>
2025-06-06 11:15:09 +0200

    fix dataset

--- qwen_bird_open/baseline.py
+++ qwen_bird_open/baseline.py
@@ -8,8 +8,9 @@ import json
 # Base path configuration
 BASE_PATH = "/home/samuele.angheben/vision-reasoning/qwen_bird_open"
 
-dataset = CUB200Dataset(split='test')
-class_names_dict = dataset.get_dataset()
+CUB200Dataset = CUB200Dataset(split='test')
+dataset = CUB200Dataset.get_dataset()
+class_names_dict = CUB200Dataset.class_names_dict
 
 model = QwenVLModel()
 prompt = "Analyze the given image and predict the most specific and accurate label possible for the primary object or scene depicted. Use scientific or technical terms when applicable to enhance specificity. If there is uncertainty about the exact label, provide a more general category or abstain from making a prediction. Ensure that all predictions are accurate and avoid guessing. The response should only contain the possible classification label, limited to a maximum of 1-3 words."
